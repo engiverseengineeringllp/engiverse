55b8d4d7bc96752cefe708d4db01f1a62e5520da
Habib Tanwir <habibtanwir1906@gmail.com>
2026-01-12 18:26:45 +0530

    removed added portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { HeroSection } from "@/components/home/HeroSection";
 import { ServicesOverview } from "@/components/home/ServicesOverview";
-import PortfolioSection from "@/components/home/PortfolioSection";
 import { AboutSection } from "@/components/home/AboutSection";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <HeroSection />
       <ServicesOverview />
-      <PortfolioSection />
       <AboutSection />
     </main>
   );
